653411e93185bc82bb35ae22c819576146b9abdd
caiovictor4884 <96833824+caiovictor4884@users.noreply.github.com>
2023-01-04 09:32:39 -0300

    concluido

---
+++ tecladator/index.js
@@ -0,0 +1,259 @@
+var audio =  document.getElementById("audio")
+
+
+
+
+
+var n1 =   document.getElementById("n1")
+
+n1.addEventListener("click", f1)
+
+function f1(){
+audio.src = "audios/ai mamae.mp3"
+}
+/////////////////////////////////////////////////////////////////////////////////////////////2
+var n2 =   document.getElementById("n2")
+
+n2.addEventListener("click", f2)
+
+function f2(){
+audio.src = "audios/ai me deus.mp3"
+}
+/////////////////////////////////////////////////////////////////////////////////////////////3
+var n3 =   document.getElementById("n3")
+
+n3.addEventListener("click", f3)
+
+function f3(){
+audio.src = "audios/ai.mp3"
+}
+/////////////////////////////////////////////////////////////////////////////////////////////4
+var n4 =   document.getElementById("n4")
+
+n4.addEventListener("click", f4)
+
+function f4(){
+audio.src = "audios/aiaiai aiii.mp3"
+}
+/////////////////////////////////////////////////////////////////////////////////////////////5
+var n5 =   document.getElementById("n5")
+
+n5.addEventListener("click", f5)
+
+function f5(){
+audio.src = "audios/arhularhularhul.mp3"
+}
+/////////////////////////////////////////////////////////////////////////////////////////////6
+var n6 =   document.getElementById("n6")
+
+n6.addEventListener("click", f6)
+
+function f6(){
+audio.src = "audios/boing.mp3"
+}
+/////////////////////////////////////////////////////////////////////////////////////////////7
+var n7 =   document.getElementById("n7")
+
+n7.addEventListener("click", f7)
+
+function f7(){
+audio.src = "audios/brincadeira.mp3"
+}
+/////////////////////////////////////////////////////////////////////////////////////////////8
+var n8 =   document.getElementById("n8")
+
+n8.addEventListener("click", f8)
+
+function f8(){
+audio.src = "audios/cavalo.mp3"
+}
+/////////////////////////////////////////////////////////////////////////////////////////////9
+var n9 =   document.getElementById("n9")
+
+n9.addEventListener("click", f9)
+
+function f9(){
+audio.src = "audios/chega.mp3"
+}
+/////////////////////////////////////////////////////////////////////////////////////////////10
+var n10 =   document.getElementById("n10")
+
+n10.addEventListener("click", f10)
+
+function f10(){
+audio.src = "audios/dança.mp3"
+}
+/////////////////////////////////////////////////////////////////////////////////////////////11
+var n11 =   document.getElementById("n11")
+
+n11.addEventListener("click", f11)
+
+function f11(){
+audio.src = "audios/demais.mp3"
+}
+/////////////////////////////////////////////////////////////////////////////////////////////12
+var n12 =   document.getElementById("n12")
+
+n12.addEventListener("click", f12)
+
+function f12(){
+audio.src = "audios/elegosta.mp3"
+}
+/////////////////////////////////////////////////////////////////////////////////////////////13
+var n13 =   document.getElementById("n13")
+
+n13.addEventListener("click", f13)
+
+function f13(){
+audio.src = "audios/galinha.mp3"
+}
+/////////////////////////////////////////////////////////////////////////////////////////////14
+var n14 =   document.getElementById("n14")
+
+n14.addEventListener("click", f14)
+
+function f14(){
+audio.src = "audios/irra.mp3"
+}
+/////////////////////////////////////////////////////////////////////////////////////////////15
+var n15 =   document.getElementById("n15")
+
+n15.addEventListener("click", f15)
+
+function f15(){
+audio.src = "audios/leão.mp3"
+}
+/////////////////////////////////////////////////////////////////////////////////////////////16
+var n16 =   document.getElementById("n16")
+
+n16.addEventListener("click", f16)
+
+function f16(){
+audio.src = "audios/não.mp3"
+}
+/////////////////////////////////////////////////////////////////////////////////////////////17
+var n17 =   document.getElementById("n17")
+
+n17.addEventListener("click", f17)
+
+function f17(){
+audio.src = "audios/ok ok.mp3"
+}
+/////////////////////////////////////////////////////////////////////////////////////////////18
+var n18 =   document.getElementById("n18")
+
+n18.addEventListener("click", f18)
+
+function f18(){
+audio.src = "audios/pare.mp3"
+}
+/////////////////////////////////////////////////////////////////////////////////////////////19
+var n19 =   document.getElementById("n19")
+
+n19.addEventListener("click", f19)
+
+function f19(){
+audio.src = "audios/potencia.mp3"
+}
+/////////////////////////////////////////////////////////////////////////////////////////////20
+var n20 =   document.getElementById("n20")
+
+n20.addEventListener("click", f20)
+
+function f20(){
+audio.src = "audios/que isso.mp3"
+}
+/////////////////////////////////////////////////////////////////////////////////////////////21
+var n21 =   document.getElementById("n21")
+
+n21.addEventListener("click", f21)
+
+function f21(){
+audio.src = "audios/risada.mp3"
+}
+/////////////////////////////////////////////////////////////////////////////////////////////22
+var n22 =   document.getElementById("n22")
+
+n22.addEventListener("click", f22)
+
+function f22(){
+audio.src = "audios/sem graça.mp3"
+}
+/////////////////////////////////////////////////////////////////////////////////////////////23
+var n23 =   document.getElementById("n23")
+
+n23.addEventListener("click", f23)
+
+function f23(){
+audio.src = "audios/somcavalo.mp3"
+}
+/////////////////////////////////////////////////////////////////////////////////////////////24
+var n24 =   document.getElementById("n24")
+
+n24.addEventListener("click", f24)
+
+function f24(){
+audio.src = "audios/tapa.mp3"
+}
+/////////////////////////////////////////////////////////////////////////////////////////////25
+var n25 =   document.getElementById("n25")
+
+n25.addEventListener("click", f25)
+
+function f25(){
+audio.src = "audios/tome.mp3"
+}
+/////////////////////////////////////////////////////////////////////////////////////////////26
+var n26 =   document.getElementById("n26")
+
+n26.addEventListener("click", f26)
+
+function f26(){
+audio.src = "audios/tõ ta bom.mp3"
+}
+/////////////////////////////////////////////////////////////////////////////////////////////26
+var n27 =   document.getElementById("n27")
+
+n27.addEventListener("click", f27)
+
+function f27(){
+audio.src = "audios/ui.mp3"
+}
+/////////////////////////////////////////////////////////////////////////////////////////////26
+var n28 =   document.getElementById("n28")
+
+n28.addEventListener("click", f28)
+
+function f28(){
+audio.src = "audios/uonuonuon.mp3"
+}
+/////////////////////////////////////////////////////////////////////////////////////////////26
+var n29 =   document.getElementById("n29")
+
+n29.addEventListener("click", f29)
+
+function f29(){
+audio.src = "audios/xiii.mp3"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
